7787d96f7cae32115fb40a1173914d89d83651fb
hellen glen <phalanxoflense@gmail.com>
2021-10-26 11:26:15 +0900

    new commit

--- main.py
+++ main.py
@@ -1,35 +1,25 @@
 import streamlit as st
-import time
+import numpy as np
+import pandas as pd
+from PIL import Image
 
 st.title("Streamlit 超入門")
-st.write("プログレスバーの表示")
 
-"Start!"
+st.write("Display Image")
 
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range(100):
-  latest_iteration.text(f"Iteration {i+1}")
-  bar.progress(i+1)
-  time.sleep(0.1)
-
-"Done!!!"
-
-left_column, right_column = st.columns(2)
-button = left_column.button("右カラムに文字を表示")
-if button:
-  right_column.write("ここは右カラムです")
-
-expander = st.expander("問い合わせ")
-expander.write("問い合わせ内容を書く")
-
-#text = st.text_input("あなたの趣味を教えてください。")
-#condition = st.slider("あなたの今の調子は？",0,100,50)
-#
-#"あなたの趣味：", text
-#"コンディション:", condition
+img = Image.open("img/untitled2.jpg")
+st.image(img, caption="flower", use_column_width=True)
 
 
 
+st.write("DataFrame")
+df = pd.DataFrame(
+  np.random.rand(100,2)/[50,50] + [35.69, 139.70],
+  columns=["lat","lon"]
+)
+st.line_chart(df) #線グラフ
+st.area_chart(df) #エリアグラフ
+st.bar_chart(df) #棒グラフ
+st.dataframe(df.style.highlight_max(axis=0)) #ハイライト
+st.map(df) #地図を表示
 
